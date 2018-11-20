6065ca71af4a07fe6473559cb2d7ad12ba0ab297
Erkmann <36168066+Erkmann@users.noreply.github.com>
2018-11-20 16:42:48 -0200

    Add files via upload

--- SFS/app/view/footer.php
+++ SFS/app/view/footer.php
@@ -1,9 +1,9 @@
 <!-- Footer -->
-<footer style="" class="page-footer font-small fixed-bottom color-dark">
+<footer style="" class="page-footer bg-info font-small color-dark">
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2018 Copyright:
-        <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    <div class="footer-copyright text-center text-muted py-3">© 2018 Copyright
+
     </div>
     <!-- Copyright -->
 
--- SFS/app/view/navbar.php
+++ SFS/app/view/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -23,13 +23,11 @@
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
+
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
 </nav>
