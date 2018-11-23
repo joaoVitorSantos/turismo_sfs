e55ee6c0f4ed4a069189c4fb3eac92a49bf177c3
Erkmann <36168066+Erkmann@users.noreply.github.com>
2018-11-23 15:11:41 -0200

    Add files via upload

--- SFS/app/controller/Home.php
+++ SFS/app/controller/Home.php
@@ -1,4 +1,25 @@
 <?php
-include_once '../view/header.php';
-include_once '../view/home.php';
-include_once '../view/footer.php';
+
+require_once '../crud/CRUD_rota.php';
+
+$c = new CRUD_rota();
+
+if (!isset($_POST['acao'])){
+
+    $c = new CRUD_rota();
+    $rotas = $c->getRotas();
+
+    include_once '../view/header.php';
+    include_once '../view/home.php';
+    include_once '../view/footer.php';
+}
+
+else {
+    switch ($_POST['acao']) {
+        case 'ver':
+            $ro = new Rota($_POST['id_rota']);
+            $res = $c->getRota($ro);
+            print_r($res);
+            break;
+    }
+}
--- SFS/app/view/home.php
+++ SFS/app/view/home.php
@@ -39,44 +39,22 @@
             <h1 class="text-center text-primary">Título</h1>
         </div>
     </div>
+    <?php foreach ($rotas as $r): ?>
+    <form method="post" action="Home.php">
     <div class="row rota">
         <div class="col-lg-8 col-md-12 text-left">
-            <div class=""><img src="../../assets/images/FOTO%20PRINCIPAL.jpg" class="img-fluid rounded imagemI imagemRota"></div>
+            <div class=""><img src="../../assets/images/<?= $r->getImagemPerfil() ?>" class="img-fluid rounded imagemI imagemRota"></div>
         </div>
         <div class="col-md-12 col-lg-4">
             <div class="texto text-left text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed nisi lacus sed viverra. Nec dui nunc mattis enim ut tellus. Malesuada nunc vel risus commodo viverra maecenas. A condimentum vitae sapien pellentesque habitant morbi tristique senectus.
+                <?= $r->getDescricao() ?>
             </div>
-            <a href="#" class="btn btn-primary btn-info">Ir!<span class="glyphicon glyphicon-circle-arrow-right"></span></a>
+            <a href="#" class=""><button type="submit" class="btn btn-primary btn-info"><?= $r->getNomeRota() ?></button><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
         </div>
-
-
-
+        <input class="text-hide" name="id_rota" value="<?= $r->getIdRota() ?>">
+        <input class="text-hide" name="acao" value="ver">
     </div>
-    <hr/>
-    <div class="row rota">
-        <div class="col-lg-8 col-md-12 text-left">
-            <div class=""><img src="../../assets/images/ROTA%203.jpg" class="img-fluid rounded imagemI imagemRota"></div>
-        </div>
-        <div class="col-md-12 col-lg-4">
-            <div class="texto text-left text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed nisi lacus sed viverra. Nec dui nunc mattis enim ut tellus. Malesuada nunc vel risus commodo viverra maecenas. A condimentum vitae sapien pellentesque habitant morbi tristique senectus.
-            </div>
-            <a href="#" class="btn btn-primary btn-info">Ir!<span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-        </div>
-    </div>
+    </form>
+    <?php endforeach; ?>
 
-    <hr/>
-
-    <div class="row rota">
-        <div class="col-lg-8 col-md-12 text-left">
-            <div class=""><img src="../../assets/images/ROTA%202%20(1).jpg" class="img-fluid rounded imagemI imagemRota"></div>
-        </div>
-        <div class="col-md-12 col-lg-4">
-            <div class="texto text-left text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed nisi lacus sed viverra. Nec dui nunc mattis enim ut tellus. Malesuada nunc vel risus commodo viverra maecenas. A condimentum vitae sapien pellentesque habitant morbi tristique senectus.
-            </div>
-            <a href="#" class="btn btn-primary btn-info">Ir!<span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-        </div>
-    </div>
 </div>
