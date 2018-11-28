40c91b577100270e4c6627e93ccc8762796e1ec3
Erkmann <erkmann08@gmail.com>
2018-11-28 15:21:33 -0200

    layout index
    ve oq tu acha mano

--- SFS/app/view/home.php
+++ SFS/app/view/home.php
@@ -33,32 +33,87 @@
 
 <div class="inicio bg-dark">
     <img class="img-fluid imagemPrincipal" src="../../assets/images/fotoprincipal.jpg" alt="">
-    <h1 class="text-img text-white rounded titulo text-center">Rotas Centro Historico</h1>
+    <h1 class="text-img text-white rounded titulo text-center">Click São Chico</h1>
 </div>
 
-<div class="container rounded">
-    <div class="row">
-        <div class="col-12">
-            <h1 class="text-center rotaTitulo">Rotas</h1>
+<div class="container contRotas rounded">
+    <div class="bgLightGray rounded-top">
+    <div class="row ">
+        <div class="col-12 text-center">
+            <h1 id="tituloR" class=" rotaTitulo">Rotas <img id="iconDrop" class="icon" src="../../assets/open_iconic/svg/caret-bottom.svg" alt="icon name"></h1>
         </div>
     </div>
+    </div>
+
+    <div class="rotas rounded-bottom">
+        <div class="row">
     <?php foreach ($rotas as $r): ?>
 
-    <form method="post" action="Home.php">
-    <div class="row rota">
-        <div class="col-lg-8 col-md-12 text-left">
-            <div class=""><img src="../../assets/images/<?= $r->getImagemPerfil() ?>" class="img-fluid rounded imagemRota"></div>
-        </div>
-        <div class="col-md-12 col-lg-4 card">
-            <div class="texto text-left card-text text-muted">
-                <?= $r->getDescricao() ?>
-            </div>
-            <a href="#" class=""><button type="submit" class="btn btn-primary btn-info"><?= $r->getNomeRota() ?></button><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-        </div>
-        <input class="text-hide" name="id_rota" value="<?= $r->getIdRota() ?>">
-        <input class="text-hide" name="acao" value="ver">
-    </div>
-    </form>
-    <?php endforeach; ?>
+        <div class="col-md-6 col-lg-4">
+            <form method="post" action="Home.php">
+            <div class="card">
+                <img class="card-img-top" src="../../assets/images/<?= $r->getImagemPerfil() ?>" alt="">
+                <div class="card-body">
+                    <h5 class="card-tittle"><?= $r->getNomeRota() ?></h5>
+                    <p class="card-text"><?= $r->getDescricao() ?></p>
+                    <button class="btn btn-outline-primary" type="submit">Ver</button>
+                </div>
 
+            </div>
+            <input class="text-hide" name="id_rota" value="<?= $r->getIdRota() ?>">
+            <input class="text-hide" name="acao" value="ver">
+            </form>
+        </div>
+
+
+    <?php endforeach; ?>
+    </div>
+    </div>
+
+    </div>
+    <div class="container contLocais rounded">
+        <div class="bgLightGray rounded-top">
+            <div class="row ">
+                <div class="col-12 text-center">
+                    <h1 id="tituloL" class="rotaTitulo">Locais <img id="iconDropL" class="icon" src="../../assets/open_iconic/svg/caret-bottom.svg" alt="icon name"></h1>
+                </div>
+            </div>
+        </div>
+
+
+    <div class="locais rounded-bottom">
+        <div class="row">
+            <?php foreach ($rotas as $r): ?>
+
+                <div class="col-md-6 col-lg-4">
+                    <form method="post" action="Home.php">
+                        <div class="card">
+                            <img class="card-img-top" src="../../assets/images/<?= $r->getImagemPerfil() ?>" alt="">
+                            <div class="card-body">
+                                <h5 class="card-tittle"><?= $r->getNomeRota() ?></h5>
+                                <p class="card-text"><?= $r->getDescricao() ?></p>
+                                <button class="btn btn-outline-primary" type="submit">Ver</button>
+                            </div>
+
+                        </div>
+                        <input class="text-hide" name="id_rota" value="<?= $r->getIdRota() ?>">
+                        <input class="text-hide" name="acao" value="ver">
+                    </form>
+                </div>
+
+
+            <?php endforeach; ?>
+        </div>
+    </div>
+    </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p class="text-left text-muted text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
 </div>
+
--- SFS/app/view/template/scriptImage.php
+++ SFS/app/view/template/scriptImage.php
@@ -1,13 +1,19 @@
 
 
     $(document).ready(function () {
+
+        $('.rotas').hide();
+        $('.locais').hide();
+
         var size = $(window).width();
 
         if (size <= 767){
             $('.imagem').removeClass('imagemI');
             $('.titulo_rota').addClass('text-center');
             $('.container').addClass('noMargin');
-            $('.titulo').attr('style', 'font-size: 200%');
+            $('.titulo').attr('style', 'font-size: 300%');
+
+
         }
 
         if(size > 767){
@@ -20,7 +26,7 @@
                 $('.titulo_rota').addClass('text-center');
                 $('.imagem').removeClass('imagemI');
                 $('.container').addClass('noMargin');
-                $('.titulo').attr('style', 'font-size: 200%');
+                $('.titulo').attr('style', 'font-size: 300%');
             }
 
             if (size >= 768){
@@ -28,9 +34,43 @@
                 $('.imagem').addClass('imagemI');
                 $('.container').removeClass('noMargin');
                 $('.titulo').attr('style', 'font-size: 500%');
-    }
+            }
 
         })
+
+        function verificaVisivel(){
+            if($('.rotas').is(':visible')){
+            $('#iconDrop').attr('src','../../assets/open_iconic/svg/caret-top.svg');
+            }
+            else{
+            $('#iconDrop').attr('src','../../assets/open_iconic/svg/caret-bottom.svg');
+            }
+        }
+
+        function verificaVisivelL(){
+            if($('.locais').is(':visible')){
+                $('#iconDropL').attr('src','../../assets/open_iconic/svg/caret-top.svg');
+            }
+            else{
+                $('#iconDropL').attr('src','../../assets/open_iconic/svg/caret-bottom.svg');
+            }
+        }
+
+        $('#tituloR').click(function(){
+            $('.rotas').slideToggle(250);
+
+            setTimeout(function(){
+                verificaVisivel();
+            }, 265);
+        });
+
+        $('#tituloL').click(function(){
+            $('.locais').slideToggle(250);
+
+            setTimeout(function(){
+                verificaVisivelL();
+            }, 265);
+        });
 
     });
 
